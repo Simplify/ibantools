cc58750d5c935b15fbd17a97b3ec444594d61b35
Saša Jovanić <sasa@simplify.ba>
2015-12-01 15:35:49 +0100

    Rewrite...

--- src/IBANTools.ts
+++ src/IBANTools.ts
@@ -1,7 +1,104 @@
 "use strict";
 
+var  countrySpecs: CountryMap = {};
 
+/**
+ * Validate IBAN
+ * @param {string} IBAN
+ * @return {boolean} valid
+ */
+export function isValidIBAN(iban: string): boolean {
+  if (iban !== undefined && iban !== null) {
+    fillSpecs();
+    var tmpIban: string = iban.replace(/[^a-zA-Z0-9]/g, "").toUpperCase();
+    var spec = countrySpecs[tmpIban.slice(0,2)];
+    if (spec !== undefined &&
+        spec.chars === tmpIban.length &&
+        checkFormatBBAN(tmpIban.slice(4), spec.bban_format) &&
+        mod9710(tmpIban) === 1) {
+      return true;
+    }
+  }
+  return false;
+}
+export interface ComposeIBANParms {
+  countryCode?: string;
+  bban?: string;
+}
+export function composeIBAN(params: ComposeIBANParms): string {
+  fillSpecs();
+  var bban: string = params.bban.replace(/[^a-zA-Z0-9]/g, "").toUpperCase();
+  var spec = countrySpecs[params.countryCode];
+  if (spec !== undefined &&
+      spec.chars === (bban.length + 4) &&
+      checkFormatBBAN(bban, spec.bban_format)) {
+		var checksom = mod9710(params.countryCode + '00' + bban);
+		return params.countryCode + ('0' + (98 - checksom)).slice(-2) + bban;
+  }
+	return null;
+}
 
+/**
+ * MOD-97-10
+ * @param {string}
+ * @return {number}
+ */
+function mod9710(iban: string): number {
+  iban = iban.slice(3) + iban.slice(0,4);
+  var validationString: string = '';
+  for (var n:number =1; n < iban.length; n++) {
+    var c = iban.charCodeAt(n);
+    if (c >= 65) {
+      validationString += (c - 55).toString();
+    } else {
+      validationString += iban[n];
+    }
+  }
+  while(validationString.length > 2) {
+    var part = validationString.slice(0,9); // 10 chars
+    validationString = (parseInt(part, 10) % 97).toString() + validationString.slice(part.length);
+  }
+  return parseInt(validationString, 10) % 97;
+}
+
+/**
+ * Check BBAN format
+ * @param {string} BBAN
+ * @param {string} BBAN format
+ */
+function checkFormatBBAN(bban: string, bformat: string): boolean {
+	if (bban.length !== bformat.length) {
+		return false;
+	}
+  for(var n = 0; n < bban.length; n++) {
+    if (bformat[n] === 'a') {
+      if (/[A-Z]/.test(bban[n])) { continue; }
+    } else if (bformat[n] === 'n' ) {
+      if (/[0-9]/.test(bban[n])) { continue; }
+    } else {
+      if (/[A-Z0-9]/.test(bban[n])) { continue; }
+    }
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Get IBAN in electronic format (no spaces)
+ * @parms {string} IBAN
+ * @return {string} IBAN or null if IBAN is not valid
+ */
+export function electonicFormatIBAN(iban: string) {
+  return iban.replace(/[^a-zA-Z0-9]/g, "").toUpperCase();
+}
+
+/**
+ * Get IBAN in friendly format (space after every 4 characters)
+ * @return {string} IBAN or null if IBAN is not valid
+ */
+export function friendlyFormatIBAN(iban: string) {
+	return electonicFormatIBAN(iban).replace(/(.{4})(?!$)/g, "$1" + ' ');
+}
 
 /**
  * Interface for IBAN Country Specification
@@ -21,186 +118,13 @@ interface CountryMap {
 }
 
 /**
- * Interface for IBANTools constructor params
+ * Fill map of IBAN country specifications
  */
-export interface IBANToolsParams {
-  iban?:        string;
-  bban?:        string;
-  countryCode?: string;
-}
-
-/** Class representing IBANTools */
-export default class IBANTools {
-  iban:        string = null;
-  bban:        string = null;
-  countryCode: string = null;
-  countryName: string = null;
-  validIBAN:   boolean = false;
-  countrySpecs: CountryMap = {};
-
-  /**
-   * Create a IBANTools
-   * @param {IBANToolsParams}
-   */
-  constructor(params: IBANToolsParams) {
-		this.fillSpecs();
-    if (params.iban !== undefined && params.iban !== null) {
-			var tmpIban: string = params.iban.replace(/[^a-zA-Z0-9]/g, "").toUpperCase();
-			if(this.isValidIBAN(tmpIban)) {
-				var spec = this.countrySpecs[tmpIban.slice(0,2)];
-				this.iban = tmpIban;
-				this.bban = this.iban.slice(4);
-				this.countryCode = this.iban.slice(0,2);
-				this.countryName = spec.name;
-				this.validIBAN = true;
-			}
-    } else if (params.bban !== undefined &&
-               params.countryCode !== undefined) {
-      this.bban = params.bban;
-      this.countryCode = params.countryCode;
-    }
-  }
-
-	/**
-	 * Extract IBAN
-	 * @param {string} IBAN
-	 */
-	extractIBAN(iban: string):IBANTools {
-		this.fillSpecs();
-    if (iban !== undefined && iban !== null) {
-			var tmpIban: string = iban.replace(/[^a-zA-Z0-9]/g, "").toUpperCase();
-			if(this.isValidIBAN(tmpIban)) {
-				var spec = this.countrySpecs[tmpIban.slice(0,2)];
-				this.iban = tmpIban;
-				this.bban = this.iban.slice(4);
-				this.countryCode = this.iban.slice(0,2);
-				this.countryName = spec.name;
-				this.validIBAN = true;
-			}
-			return this;
-    }
-	}
-
-	/**
-	 * Validate IBAN
-	 * @param {string} IBAN
-	 * @return {boolean} valid
-	 */
-	isValidIBAN(iban: string): boolean {
-    if (iban !== undefined && iban !== null) {
-			this.fillSpecs();
-			var tmpIban: string = iban.replace(/[^a-zA-Z0-9]/g, "").toUpperCase();
-			var spec = this.countrySpecs[tmpIban.slice(0,2)];
-			if (spec !== undefined &&
-					spec.chars === tmpIban.length &&
-					this.checkFormatBBAN(tmpIban.slice(4), spec.bban_fields) &&
-					this.mod9710(tmpIban) === 1) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-  /**
-   * Get IBAN validity
-   * @return {boolean}
-   */
-  isValid() {
-    return this.validIBAN;
-  }
-
-  /**
-   * Get IBAN in electronic format (no spaces)
-   * @return {string} IBAN or null if IBAN is not valid
-   */
-  getIBAN() {
-    return this.iban;
-  }
-
-  /**
-   * Get IBAN in friendly format (space after every 4 characters)
-   * @return {string} IBAN or null if IBAN is not valid
-   */
-  getFriendlyIBAN() {
-    return this.iban.replace(/(.{4})(?!$)/g, "$1" + ' ');
-  }
-
-  /**
-   * Get BBAN in electronic format (no spaces)
-   * @return {string} BBAN or null if IBAN is not valid
-   */
-  getBBAN() {
-    return this.bban;
-  }
-
-  /**
-   * Get country code in ISO 3166-1 alpha-2
-   * @return {string} Country Code or null if IBAN is not valid
-   */
-  getCountryCode() {
-    return this.countryCode;
-  }
-
-  /**
-   * Get Country
-   * @return {string} Country or null if IBAN is not valid
-   */
-  getCountryName() {
-    return this.countryName;
-  }
-
-	/**
-	 * MOD-97-10
-	 * @param {string}
-	 * @return {number}
-	 */
-	mod9710(iban: string): number {
-		iban = iban.slice(3) + iban.slice(0,4);
-		var validationString: string = '';
-		for (var n:number =1; n < iban.length; n++) {
-			var c = iban.charCodeAt(n);
-			if (c >= 65) {
-				validationString += (c - 55).toString();
-			} else {
-				validationString += iban[n];
-			}
-		}
-		while(validationString.length > 2) {
-			var part = validationString.slice(0,9); // 10 chars
-			validationString = (parseInt(part, 10) % 97).toString() + validationString.slice(part.length);
-		}
-		return parseInt(validationString, 10) % 97;
-	}
-
-	/**
-	 * Check BBAN format
-	 * @param {string} BBAN
-	 * @param {string} BBAN format
-	 */
-	checkFormatBBAN(bban: string, format: string): boolean {
-		for(var n:number = 0; n < format.length; n++) {
-			if (format[n] === 'a') {
-				if (/[A-Z]/.test(bban[n])) { continue; }
-			} else if (format[n] === 'n' ) {
-				if (/[0-9]/.test(bban[n])) { continue; }
-			} else {
-				if (/[A-Z0-9]/.test(bban[n])) { continue; }
-			}
-			return false;
-		}
-		return true;
-	}
-
-  /**
-   * Fill map of IBAN country specifications
-   */
-  fillSpecs() {
-		// INSERT_START
-		this.countrySpecs['BA'] = {chars: 20, bban_format: 'nnnnnnnnnnnnnnnn', bban_fields: 'bbbsssccccccccxx', name: 'Bosnia and Herzegovina'};
-		this.countrySpecs['BR'] = {chars: 29, bban_format: 'nnnnnnnnnnnnnnnnnnnnnnnac', bban_fields: 'bbbbbbbbssssscccccccccctn', name: 'Brazil'};
-		this.countrySpecs['NL'] = {chars: 18, bban_format: 'aaaannnnnnnnnn', bban_fields: 'bbbbcccccccccc', name: 'Netherlands'};
-		this.countrySpecs['MK'] = {chars: 19, bban_format: 'nnnccccccccccnn', bban_fields: 'bbbccccccccccxx', name: 'Republic of Macedonia'};
-
-		// INSERT_END
-  }
+function fillSpecs() {
+  // INSERT_START
+  countrySpecs['BA'] = {chars: 20, bban_format: 'nnnnnnnnnnnnnnnn', bban_fields: 'bbbsssccccccccxx', name: 'Bosnia and Herzegovina'};
+  countrySpecs['BR'] = {chars: 29, bban_format: 'nnnnnnnnnnnnnnnnnnnnnnnac', bban_fields: 'bbbbbbbbssssscccccccccctn', name: 'Brazil'};
+  countrySpecs['NL'] = {chars: 18, bban_format: 'aaaannnnnnnnnn', bban_fields: 'bbbbcccccccccc', name: 'Netherlands'};
+  countrySpecs['MK'] = {chars: 19, bban_format: 'nnnccccccccccnn', bban_fields: 'bbbccccccccccxx', name: 'Republic of Macedonia'};
+  // INSERT_END
 }
--- test/IBANToolsTest.ts
+++ test/IBANToolsTest.ts
@@ -5,113 +5,89 @@
 import chai = require('chai');
 var expect = chai.expect;
 
-import IBANTools from '../src/IBANTools';
+//import iban from '../src/IBANTools';
+import iban = require('../src/IBANTools');
 
 describe('IBANTools', () => {
 
-  describe('When calling isValidIBAN() with valid IBAN', () => {
-	  it('isValidIBAN() should return true', () => {
-			expect(new IBANTools({}).isValidIBAN('NL91 ABNA 0417 1643 00')).to.be.true;
-		});
-	});
-  describe('When calling isValidIBAN() with invalid IBAN', () => {
-	  it('isValidIBAN() should return false', () => {
-			expect(new IBANTools({}).isValidIBAN('NL91 ABNA 0517 1643 00')).to.be.false;
-		});
-	});
-  describe('When calling extractIBAN() with valid IBAN', () => {
-		var subject = new IBANTools({}).extractIBAN('NL91 ABN A0 41 716 43 00');
-	  it('isValid() should return true', () => {
-			expect(subject.isValid()).to.be.true;
-		});
-		it('getBBAN() should return ABNA0417164300', () => {
-			expect(subject.getBBAN()).to.equal('ABNA0417164300');
-		});
-	  it('getIBAN() should return NL91ABNA0417164300', () => {
-			expect(subject.getIBAN()).to.equal('NL91ABNA0417164300');
-		});
-	  it('getCountryCode() should return NL', () => {
-			expect(subject.getCountryCode()).to.equal('NL');
-		});
-	  it('getCountryName() should return Netherlands', () => {
-			expect(subject.getCountryName()).to.equal('Netherlands');
-		});
-	});
+  describe('When calling isValidIBAN()', () => {
+    it('with valid IBAN should return true', () => {
+      expect(iban.isValidIBAN('NL91 ABNA 0417 1643 00')).to.be.true;
+    });
+    it('with invalid IBAN should return false', () => {
+      expect(iban.isValidIBAN('NL91 ABNA 0517 1643 00')).to.be.false;
+    });
+    it('with no IBAN should return false', () => {
+      expect(iban.isValidIBAN(null)).to.be.false;
+    });
+  });
+  describe('When calling composeIBAN()', () => {
+    it('with valid country code and valid BBAN should return NL91ABNA0417164300', () => {
+      expect(iban.composeIBAN({countryCode: 'NL', bban: 'ABNA0417164300'})).to.equal('NL91ABNA0417164300');
+    });
+    it('with invalid country code and valid BBAN should return null', () => {
+      expect(iban.composeIBAN({countryCode: 'ZZ', bban: 'ABNA0417164300'})).to.be.null;
+    });
+    it('with valid country code and invalid BBAN (alpha) should return null', () => {
+      expect(iban.composeIBAN({countryCode: 'NL', bban: 'A7NA0417164300'})).to.be.null;
+    });
+    it('with valid country code and invalid BBAN (num) should return null', () => {
+      expect(iban.composeIBAN({countryCode: 'NL', bban: 'ABNA04171Z4300'})).to.be.null;
+    });
+  });
 
-	describe('When initialized with empty object', () => {
-		var subject = new IBANTools({});
-	  it('isValid() should return false', () => {
-			expect(subject.isValid()).to.be.false;
-		});
-	  it('getBBAN() should return null', () => {
-			expect(subject.getBBAN()).to.be.null;
-		});
-	  it('getIBAN() should return null', () => {
-			expect(subject.getIBAN()).to.be.null;
-		});
-	  it('getCountryCode() should return null', () => {
-			expect(subject.getCountryCode()).to.be.null;
-		});
-	  it('getCountryName() should return null', () => {
-			expect(subject.getCountryName()).to.be.null;
-		});
-	});
 
-	describe('When initialized with null IBAN', () => {
-		var subject = new IBANTools({iban: null});
-	  it('isValid() should return false', () => {
-			expect(subject.isValid()).to.be.false;
-		});
-	});
+/*
+	describe('When calling extractIBAN() with valid IBAN', () => {
+    var subject = new IBANTools({}).extractIBAN('NL91 ABN A0 41 716 43 00');
+    it('isValid() should return true', () => {
+      expect(subject.isValid()).to.be.true;
+    });
+    it('getBBAN() should return ABNA0417164300', () => {
+      expect(subject.getBBAN()).to.equal('ABNA0417164300');
+    });
+    it('getIBAN() should return NL91ABNA0417164300', () => {
+      expect(subject.getIBAN()).to.equal('NL91ABNA0417164300');
+    });
+    it('getCountryCode() should return NL', () => {
+      expect(subject.getCountryCode()).to.equal('NL');
+    });
+    it('getCountryName() should return Netherlands', () => {
+      expect(subject.getCountryName()).to.equal('Netherlands');
+    });
+  });
 
   describe('When initialized with valid Dutch IBAN', () => {
-		var subject = new IBANTools({iban: 'NL91 ABNA 0417 1643 00'});
-	  it('isValid() should return true', () => {
-			expect(subject.isValid()).to.be.true;
-		});
-	  it('getBBAN() should return ABNA0417164300', () => {
-			expect(subject.getBBAN()).to.equal('ABNA0417164300');
-		});
-	  it('getIBAN() should return NL91ABNA0417164300', () => {
-			expect(subject.getIBAN()).to.equal('NL91ABNA0417164300');
-		});
-	  it('getCountryCode() should return NL', () => {
-			expect(subject.getCountryCode()).to.equal('NL');
-		});
-	  it('getCountryName() should return Netherlands', () => {
-			expect(subject.getCountryName()).to.equal('Netherlands');
-		});
-	});
+    var subject = new IBANTools({iban: 'NL91 ABNA 0417 1643 00'});
+    it('isValid() should return true', () => {
+      expect(subject.isValid()).to.be.true;
+    });
+    it('getBBAN() should return ABNA0417164300', () => {
+      expect(subject.getBBAN()).to.equal('ABNA0417164300');
+    });
+    it('getIBAN() should return NL91ABNA0417164300', () => {
+      expect(subject.getIBAN()).to.equal('NL91ABNA0417164300');
+    });
+    it('getCountryCode() should return NL', () => {
+      expect(subject.getCountryCode()).to.equal('NL');
+    });
+    it('getCountryName() should return Netherlands', () => {
+      expect(subject.getCountryName()).to.equal('Netherlands');
+    });
+  });
 
-  describe('When initialized with invalid Dutch IBAN', () => {
-		var subject = new IBANTools({iban: 'NL92 ABNA 0417 1643 00'});
-	  it('isValid() should return false', () => {
-			expect(subject.isValid()).to.be.false;
-		});
-	  it('getBBAN() should return null', () => {
-			expect(subject.getBBAN()).to.be.null;
-		});
-	  it('getIBAN() should return null', () => {
-			expect(subject.getIBAN()).to.be.null;
-		});
-	});
-  describe('When initialized with valid Macedonian IBAN', () => {
-		var subject = new IBANTools({iban: 'MK07 2501 2000 0058 984'});
-	  it('isValid() should return true', () => {
-			expect(subject.isValid()).to.be.true;
-		});
-	});
   describe('When initialized with valid Brazilian IBAN', () => {
-		var subject = new IBANTools({iban: 'BR97 0036 0305 0000 1000 9795 493P 1'});
-	  it('isValid() should return true', () => {
-			expect(subject.isValid()).to.be.true;
-		});
-	  it('getIban() should return BR9700360305000010009795493P1', () => {
-			expect(subject.getIBAN()).to.equal('BR9700360305000010009795493P1');
-		});
-	  it('getFriendlyIban() should return BR97 0036 0305 0000 1000 9795 493P 1', () => {
-			expect(subject.getFriendlyIBAN()).to.equal('BR97 0036 0305 0000 1000 9795 493P 1');
-		});
-	});
-
+    var subject = new IBANTools({iban: 'BR97 0036 0305 0000 1000 9795 493P 1'});
+    it('isValid() should return true', () => {
+      expect(subject.isValid()).to.be.true;
+    });
+    it('getIban() should return BR9700360305000010009795493P1', () => {
+      expect(subject.getIBAN()).to.equal('BR9700360305000010009795493P1');
+    });
+    it('getFriendlyIban() should return BR97 0036 0305 0000 1000 9795 493P 1', () => {
+      expect(subject.getFriendlyIBAN()).to.equal('BR97 0036 0305 0000 1000 9795 493P 1');
+    });
+  });
+*/
 });
+
