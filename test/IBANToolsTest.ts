f8d70ddeaa55917d5543e411da095198397e7b0e
Saša Jovanić <sasa@simplify.ba>
2015-12-02 13:11:01 +0100

    Added license to files

--- test/IBANToolsTest.ts
+++ test/IBANToolsTest.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /// <reference path="../typings/mocha/mocha.d.ts" />
 /// <reference path="../typings/chai/chai.d.ts" />
 /// <reference path="../src/IBANTools.ts" />
