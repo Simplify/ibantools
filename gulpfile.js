06da8eb3d3877023676432724d45e829d1c60282
Saša Jovanić <sasa@simplify.ba>
2015-12-02 12:50:21 +0100

    Tests moved to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var shell = require('gulp-shell');
+const mocha = require('gulp-mocha');
 
 gulp.task('default', ['ts', 'tts', 'watch']);
 
 gulp.task('doc', shell.task([
 	'./node_modules/.bin/jsdoc src/IBANTools.js -d docs -r README.md'
 ]));
-
 
 // Compile typescript sources
 gulp.task('ts', function() {
@@ -25,7 +25,14 @@ gulp.task('tts', function() {
     .pipe(gulp.dest('./test'));
 });
 
+// Run tests
+gulp.task('test', function() {
+	return gulp.src(['test/**/*.js'], {read: false})
+		.pipe(mocha());
+});
+
+// Watch for changes
 gulp.task('watch', function() {
-  gulp.watch('./src/**/*.ts', ['ts']);
-  gulp.watch('./test/**/*.ts', ['tts']);
+  gulp.watch('./src/**/*.ts', ['ts','tts','test']);
+  gulp.watch('./test/**/*.ts', ['tts','ts','test']);
 });
